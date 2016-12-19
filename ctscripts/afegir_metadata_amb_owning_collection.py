e221e507c9e4825cfa68360703f7c14c60756ece
Joan Caparrós <joan.caparros@csuc.cat>
2016-12-19 16:25:57 +0100

    Add new file

---
+++ ctscripts/afegir_metadata_amb_owning_collection.py
@@ -0,0 +1,24 @@
+from org.dspace.curate import ScriptedTask
+from org.dspace.curate import Curator
+from org.dspace.content.service import DSpaceObjectService
+from org.dspace.content.factory import ContentServiceFactory
+
+#from org.dspace.content.service import BitstreamService
+ 
+class MT(ScriptedTask):
+        def init(self, curator, taskName):
+                print "initializing with Jython"
+ 
+        def performDso(self, dso):
+                #print "perform on dso "
+                if dso.getType()==2:
+                        print "Item '" + dso.getName() + "' ("+dso.
+getHandle()+")"
+                	col= dso.getOwningCollection().getName()
+			dso.itemService.addMetadata(Curator.curatio
+nContext(),dso , "dc", "relation", "ispartofseries", dso.ANY, col);
+		return 0
+ 
+        def performId(self, context, id):
+                print "perform on id %s" % (id)
+                return 0
